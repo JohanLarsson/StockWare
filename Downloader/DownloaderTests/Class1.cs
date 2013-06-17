ebbc6af39c228f77e833299c355621dffe810b85
Johan Larsson <johan_gavle1@hotmail.com>
2013-06-17 23:22:28 +0200

    Downloaded something

--- Downloader/DownloaderTests/Class1.cs
+++ Downloader/DownloaderTests/Class1.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace DownloaderTests
 {
     public class Class1
     {
+        [Test]
+        public void GetSomethingTest()
+        {
+            var webClient = new WebClient();
+            var downloadString = webClient.DownloadString(@"http://finance.yahoo.com/d/quotes.csv?s=XOM+BBDb.TO+JNJ+MSFT&f=snd1l1yr");
+        }
     }
 }
