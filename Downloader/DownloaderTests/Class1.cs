cbcdada03f3f857afb10d85f4131aab8b083929f
Johan Larsson <johan_gavle1@hotmail.com>
2013-06-17 23:15:54 +0200

    Initial

---
+++ Downloader/DownloaderTests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloaderTests
+{
+    public class Class1
+    {
+    }
+}
