82715d121cd62e06fd2585ba591ecb45def74252
Johan Larsson <johan_gavle1@hotmail.com>
2013-06-18 21:15:17 +0200

    looks like the historicaldata table is down, lets hope it is temporary

--- Downloader/Downloader/DownloaderTests/YqlTests.cs
+++ Downloader/Downloader/DownloaderTests/YqlTests.cs
@@ -19,7 +19,7 @@ namespace DownloaderTests
         {
             var webClient = new WebClient();
             string yahooFinanceQuotes = @"yahoo.finance.quotes";
-            var query = string.Format(@"select * from {0} where symbol=""{1}""", yahooFinanceQuotes, symbol);
+            var query = string.Format(@"select * from {0} where symbol='{1}'", yahooFinanceQuotes, symbol);
             var url = _baseAddress + Uri.EscapeDataString(query) + _json + _tables + _callback;
             Console.WriteLine(url);
             var downloadString = webClient.DownloadString(url);
@@ -28,7 +28,7 @@ namespace DownloaderTests
 
             RootObject<QuoteResults> deserialize = downloadString.Deserialize<RootObject<QuoteResults>>();
 
-            Console.WriteLine(deserialize.Query.Count);
+            Assert.AreEqual(1, deserialize.Query.Count);
         }
 
         [TestCase("yhoo")]
@@ -45,7 +45,26 @@ namespace DownloaderTests
 
             var deserialize = downloadString.Deserialize<RootObject<OptionsChainResults>>();
 
-            Console.WriteLine(deserialize.Query.Count);
+            Assert.AreEqual(1, deserialize.Query.Count);
+        }
+
+        [TestCase("yhoo")]
+        public void GetHistoricalDataTest(string symbol)
+        {
+            var webClient = new WebClient();
+            string yahooFinanceHistoricaldata = @"yahoo.finance.historicaldata";
+            string startDate = @"2009-09-11";
+            string endDate = @"2010-03-10";
+            var query = string.Format(@"select * from {0} where startDate = '{2}' and symbol = '{1}' and endDate = '{3}'", yahooFinanceHistoricaldata, symbol, startDate, endDate);
+            var url = _baseAddress + Uri.EscapeDataString(query) + _json + _tables + _callback;
+            Console.WriteLine(url);
+            var downloadString = webClient.DownloadString(url);
+            Console.WriteLine();
+            Console.WriteLine(downloadString);
+
+            var deserialize = downloadString.Deserialize<RootObject<OptionsChainResults>>();
+
+            Assert.AreEqual(1,deserialize.Query.Count);
         }
 
     }
