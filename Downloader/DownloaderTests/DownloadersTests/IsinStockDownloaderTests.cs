267db98b9f21b46bac1fc568d84009c36489a316
JohanLarsson <johan_gavle1@hotmail.com>
2013-06-19 17:01:27 +0200

    Playing around with proxy for https

--- Downloader/DownloaderTests/DownloadersTests/IsinStockDownloaderTests.cs
+++ Downloader/DownloaderTests/DownloadersTests/IsinStockDownloaderTests.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using Downloader.Dtos;
 using Downloader.Yahoo.Finance.Downloaders;
 using Downloader.Yahoo.Finance.Dtos;
 using NUnit.Framework;
@@ -8,22 +7,22 @@ using NUnit.Framework;
 namespace DownloaderTests.DownloadersTests
 {
     [Explicit]
-    public class StockDownloaderTests
+    public class IsinStockDownloaderTests
     {
-        [TestCase("yhoo")]
+        [TestCase("US9843321061")]
         public async void DownloadOneTest(string symbol)
         {
-            var downloader = new StockDownloader();
-            Stock download = await downloader.Download(symbol);
+            var downloader = new IsinStockDownloader();
+            IsinStock download = await downloader.Download(symbol);
             Assert.IsTrue(symbol.Equals(download.Symbol, StringComparison.OrdinalIgnoreCase));
         }
 
-        static string[] Symbols = new[] { "yhoo", "aapl", "goog", "msft" };
+        static string[] Symbols = new[] { "US9843321061", "US0378331005" };
         [Test]
         public async void DownloadManyTest()
         {
-            var downloader = new StockDownloader();
-            List<Stock> download = await downloader.Download(Symbols);
+            var downloader = new IsinStockDownloader();
+            List<IsinStock> download = await downloader.Download(Symbols);
             Assert.AreEqual(Symbols.Length, download.Count);
         }
     }
---
+++ Downloader/DownloaderTests/ProxyTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DownloaderTests
+{
+    class ProxyTests
+    {
+        [Test]
+        public void ProxyHttpTest()
+        {
+            var client = new WebClient();
+            string httpUrl = @"http://en.wikipedia.org/wiki/Main_Page";
+            string withoutProxy = client.DownloadString(httpUrl);
+
+            IWebProxy proxy = WebRequest.DefaultWebProxy;
+            proxy.Credentials = CredentialCache.DefaultCredentials;
+            client.Proxy = proxy;
+            var withProxy = client.DownloadString(httpUrl);
+            //Assert.AreEqual(withProxy,withoutProxy);
+        }
+
+        [Test]
+        public void ProxyHttpsTest()
+        {
+            var client = new WebClient();
+            string httpsUrl = @"https://api.test.nordnet.se/next/login";
+            var exception = Assert.Throws<WebException>(() => { string withoutProxy = client.DownloadString(httpsUrl); });
+            Console.WriteLine(exception.Message); // The remote server returned an error: (407) Proxy Authentication Required.
+
+            IWebProxy proxy = WebRequest.DefaultWebProxy;
+            proxy.Credentials = CredentialCache.DefaultCredentials;
+            client.Proxy = proxy;
+            var webException = Assert.Throws<WebException>(() => { var withProxy = client.DownloadString(httpsUrl); });
+            Console.WriteLine(webException.Message); // The remote server returned an error: (403) Forbidden.
+        }
+
+        [Test]
+        public void ProxyUrisTest()
+        {
+            IWebProxy proxy = WebRequest.DefaultWebProxy;
+            Uri uriHttp = proxy.GetProxy(new Uri(@"http://en.wikipedia.org/wiki/Main_Page"));
+            Uri uriHttps = proxy.GetProxy(new Uri(@"https://api.test.nordnet.se/next/login"));
+            Assert.AreEqual(uriHttp, uriHttps);
+        }
+    }
+}
